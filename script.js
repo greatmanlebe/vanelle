aec6e6320cac9a788e4f224cb456f05d6c39a4cf
greatmanlebe <lebegreatman00@gmail.com>
2026-02-02 21:31:34 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css 	new file:   us.png

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const message = document.getElementById("message");
 const question = document.getElementById("question");
-
+const valentineImg = document.getElementById("valentineImg");
 ["mouseover", "touchstart"].forEach(event => {
     noBtn.addEventListener(event, () => {
         const x = Math.random() * 160 - 80;
@@ -19,7 +19,7 @@ yesBtn.addEventListener("click", () => {
     message.innerHTML = 
         "Tu viens de me rendre le plus heureux ❤️<br>" +
         "Fais un screenshot et envoie-le moi 😘";
-
+    valentineImg.style.display = "block";
     yesBtn.style.display = "none";
     noBtn.style.display = "none";
 });

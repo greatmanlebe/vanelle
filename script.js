68da3282cc6f0131e63333ec6ece6dfb5539b7e0
greatmanlebe <lebegreatman00@gmail.com>
2026-02-02 21:07:02 +0300

    new file:   index.html new file:   script.js new file:   style.css

---
+++ script.js
@@ -0,0 +1,25 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+const question = document.getElementById("question");
+
+["mouseover", "touchstart"].forEach(event => {
+    noBtn.addEventListener(event, () => {
+        const x = Math.random() * 160 - 80;
+        const y = Math.random() * 80 - 40;
+
+        noBtn.style.transform = `translate(${x}px, ${y}px)`;
+        message.textContent =
+            "😏 Si tu n'arrives pas à appuyer sur NON… alors appuie sur OUI 💖";
+    });
+});
+
+yesBtn.addEventListener("click", () => {
+    question.textContent = "💖 Je t'aime Vanelle 💖";
+    message.innerHTML = 
+        "Tu viens de me rendre le plus heureux ❤️<br>" +
+        "Fais un screenshot et envoie-le moi 😘";
+
+    yesBtn.style.display = "none";
+    noBtn.style.display = "none";
+});
